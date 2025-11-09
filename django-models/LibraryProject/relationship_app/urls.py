2f9bb3ee92f4cc69870c28cd0071980b77dcafe3
fkde17 <75939288+fkde17@users.noreply.github.com>
2025-11-09 14:48:49 +0300

    feat: add register, LoginView, LogoutView with template_name in urls.py

--- django-models/LibraryProject/relationship_app/urls.py
+++ django-models/LibraryProject/relationship_app/urls.py
@@ -1,22 +1,19 @@
 # relationship_app/urls.py
 from django.urls import path
-from .views import list_books
+from django.contrib.auth import views as auth_views
 from . import views
 
 app_name = 'relationship_app'
 
 urlpatterns = [
-    # Function-based view
-    path('books/', views.list_books, name='list_books'),
 
-    # Class-based view
-    path('library/<int:pk>/', views.LibraryDetailView.as_view(), name='library_detail'),
-    path('admin/', views.admin_view, name='admin_view'),
-    path('librarian/', views.librarian_view, name='librarian_view'),
-    path('member/', views.member_view, name='member_view'),
+    path('register/', views.register, name='register'),
+    path('login/', auth_views.LoginView.as_view(template_name='relationship_app/login.html'), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(template_name='relationship_app/logout.html'), name='logout'),
+
+
     path('add_book/', views.add_book, name='add_book'),
     path('edit_book/<int:pk>/', views.edit_book, name='edit_book'),
     path('delete_book/<int:pk>/', views.delete_book, name='delete_book'),
     path('books/', views.list_books, name='book_list'),
-
 ]
